6c70327a495b0ffd8133bbbe3768cad7a12382a9
edmund <eddableheath@gmail.com>
2025-03-22 15:40:17 -0400

    :tada: set up package structure

---
+++ src/vor-cell-vis/__init__.py
@@ -0,0 +1,10 @@
+"""
+Voronoi Cell Visuals: Creates visuals based on depth within images, creating voronoi cells with greater resolution for closer parts of the frame.
+"""
+
+from __future__ import annotations
+
+from importlib.metadata import version
+
+__all__ = ("__version__",)
+__version__ = version(__name__)
